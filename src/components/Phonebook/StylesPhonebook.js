38dfeb64e541b146e0cfaa66a674e4b324dab603
Andrii-Smolianinov <andr380v@gmail.com>
2022-09-21 03:24:43 +0300

    add styles

--- src/components/Phonebook/StylesPhonebook.js
+++ src/components/Phonebook/StylesPhonebook.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const StylesPhonebook = styled.div`
+ display: flex;
+  flex-direction: column; 
+  align-items: center;
   margin: 40px;
   font-weight: 500;
   font-size: 16px;
